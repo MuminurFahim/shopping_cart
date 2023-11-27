f5b2a37b326861aeedbf40015c96e7bd7f6e27b8
Muminur Fahim <121749007+MuminurFahim@users.noreply.github.com>
2023-11-27 16:28:48 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,13 @@
 function add_to_cart (fruit, price) {
-    let pre_row = document.getElementById(fruit);
-    if (!pre_row) {
-        let table = document.getElementById('cart'),
-
-        row = document.createElement('tr'),
-        col1 = document.createElement('td'),
-        col2 = document.createElement('td'),
-        col3 = document.createElement('td'),
-        btn = document.createElement('button');
-    
-        btn.addEventListener('click', ()=>{
-            row.remove();
-        })
-        
-        row.setAttribute('id', fruit);
-        col2.setAttribute('class', 'price');
-    
-        col1.textContent = fruit;
-        col2.textContent = '$' + price;
-        btn.textContent = 'Remove';
-    
-        row.appendChild(col1);
-        row.appendChild(col2);
-        col3.appendChild(btn);
-        row.appendChild(col3);
-        table.appendChild(row);    
+    if (!document.getElementById(fruit)) {
+        const table = document.getElementById('cart');
+        table.innerHTML += `
+        <tr id=${fruit}>
+            <td>${fruit}</td>
+            <td class='price'>$${price}</td>
+            <td>
+                <button onclick='${fruit}.remove()'>Remove</button>
+            </td>
+        </tr>`;
     }
 }
