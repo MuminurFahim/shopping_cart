da65b56b7a31c892379e156487eb3c8c593f766b
Muminur Fahim <121749007+MuminurFahim@users.noreply.github.com>
2023-11-29 20:19:17 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 function add_to_cart(fruit, price) {
     if (!document.getElementById(fruit)) {
-        document.getElementById('cart').innerHTML += `
-            <tr id=${fruit}>
-                <td>${fruit}</td>
-                <td class='price'>$${price}</td>
-                <td>
-                    <button onclick='${fruit}.remove()'>Remove</button>
-                </td>
-            </tr>`;
+        const row = document.createElement('tr');
+        row.setAttribute('id', fruit);
+        row.innerHTML = `
+            <td>${fruit}</td>
+            <td class='price'>$${price}</td>
+            <td>
+                <button onclick='${fruit}.remove()'>Remove</button>
+            </td>`;
+        document.getElementById('cart').appendChild(row);
     }
 }
